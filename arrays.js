77bea30ea1c5401e90cf0007904fd5f75cb134cc
t dis <thomdisley@gmail.com>
2017-09-19 11:31:40 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,39 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+
+addElementToBeginningOfArray = function(array, element) {
+  var newArray = [element, ...array]
+  return newArray;
+}
+
+destructivelyAddElementToBeginningOfArray = function(array, element) {
+  return array.unshift(element);
+}
+
+// destructivelyAddElementToBeginningOfArray(chocolateBars, 'snic');
+//
+// console.log(chocolateBars);
+
+
+addElementToEndOfArray = function(array, element) {
+  var newArray = [...array, element]
+  return newArray;
+}
+
+destructivelyAddElementToBeginningOfArray = function(array, element) {
+  return array.push(element);
+}
+
+accessElementInArray = function (array, position) {
+  return array[position];
+}
+
+destructivelyRemoveElementFromBeginningOfArray = function (array) {
+  array.shift;
+  return array;
+}
+
+removeElementFromBeginningOfArray = function(array) {
+  var first = array.slice(0,1);
+  return array;
+}
